069692a27b29588e2c768990a0300f3d2a7f05f2
Hager Abobakr <hagarabobakr1@gmail.com>
2024-08-24 07:43:16 +0300

    some modifications

--- app/src/main/java/com/example/foodplanner/model/database/AppDatabase.java
+++ app/src/main/java/com/example/foodplanner/model/database/AppDatabase.java
@@ -21,7 +21,7 @@ public abstract class AppDatabase extends RoomDatabase {
     public static synchronized AppDatabase getInstance(Context context) {
         if (instance == null) {
             instance = Room.databaseBuilder(context.getApplicationContext(),
-                            AppDatabase.class, "meal_database")
+                            AppDatabase.class, "meals_database")
                     .fallbackToDestructiveMigration()
                     .build();
         }
